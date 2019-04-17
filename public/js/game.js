1d9a7718283f5378b4b1775a07a74fd62100c960
Unknown <cjc1288@naver.com>
2019-04-18 01:15:12 +0900

    resolve # 187

--- public/js/game.js
+++ public/js/game.js
@@ -180,7 +180,7 @@ var Game = {
 
         for (var y = 0; y < 3; y++) {
             for (var x = 0; x < 5; x++) {
-                var alien = aliens.create(x * 48, Math.floor(Math.random() * 325), 'invader');
+                var alien = aliens.create(x * 56, Math.floor(Math.random() * 325) , 'invader');
                 alien.anchor.setTo(0.5, 0.5);
                 alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
                 alien.play('fly');
@@ -279,7 +279,7 @@ var Game = {
         explosion.reset(player.body.x, player.body.y);
         explosion.play('kaboom', 30, false, true);
     
-        // ?? ?´?´ê°? ì£½ê±°? ? ?´ ?¤ ì£½ì ?
+        // ?��?��?��?���? 죽거?�� ?��?�� ?�� 죽을 ?��
         if ((lives.countLiving() < 1) || (aliens.countLiving() == 0)) {
             this.finishGame();
         }
