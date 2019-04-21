636e31f2f35722efb9afc7265033d4e648cd9129
DONGHYUN GOH <aldehf420@naver.com>
2019-04-22 01:08:23 +0900

    merge

--- public/js/game.js
+++ public/js/game.js
@@ -229,11 +229,7 @@ var Game = {
             }
 
             //speedUp
-<<<<<<< HEAD
             if((random*10) >=30 && (random*10)<50){
-=======
-            if((random*10) >=30 && (random*10) <50){
->>>>>>> add-item
                 if(item_count <1){
                     item_count +=1;
                     var speedup_1 = speedup.create(Math.random() * (game.width/2), 100 + Math.random() * (game.height-200),'speedup');
