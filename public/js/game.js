d08f2cb0c6804304fa3edc4d6c6a96fd686844bd
Jaesun Park <zzangjames75@gmail.com>
2019-04-22 18:33:45 +0900

    Auto stash before merge of "master" and "upstream/master"

--- public/js/game.js
+++ public/js/game.js
@@ -43,8 +43,8 @@ var Game = {
         game.load.spritesheet('kaboom', 'img/explode.png', 128, 128);
         game.load.image('starfield', 'img/starfield.png');
         game.load.image('heart', 'img/heart.png');
-        game.load.image('upper_mountain', 'img/upper_mountain.png');
         game.load.image('lower_mountain', 'img/lower_mountain.png');
+        game.load.image('upper_mountain', 'img/upper_mountain.png');
         // load all sfx and music
         game.load.audio('music1', 'audio/gradius.mp3');
         game.load.audio('sfx_enemy_die', 'audio/enemy-die.wav');
@@ -252,7 +252,7 @@ var Game = {
 
     createAliens : function() {
 
-        for (var i = 0; i < stage*3; i++) {
+        for (var i = 0; i < 15; i++) {
             var alien = aliens.create(Math.random() * 290, Math.random() * 540, 'invader');
             alien.anchor.setTo(0.5, 0.5);
             alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
@@ -334,7 +334,8 @@ var Game = {
             countstage++;
             stage++;
             stageText.text = stageString + stage;
-
+		
+            
         }
     },
 
@@ -472,3 +473,4 @@ var Game = {
     }
 
 }
+
