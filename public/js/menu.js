d410180fedc1ab9d1492c3b1b04b5d6b81ac137e
js desktop <ppq23@naver.com>
2019-04-23 22:04:54 +0900

    refs #406 controls

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,11 +4,14 @@ var mainMenu = {
 
     preload : function() {
         game.load.image('startButton', 'img/startbutton.png');
+        game.load.image('controls', 'img/controls.png');       
+        game.load.image('controlkey', 'img/controlkey.png');
     },
 
     create : function() {
         game.stage.backgroundColor = '#fff';
         game.add.button(350,270,'startButton', this.startGame, this);
+        game.add.button(390,330,'controls', this.ViewControls, this);
         easyStart = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     },
 
@@ -20,6 +23,10 @@ var mainMenu = {
 
     startGame : function() {
         this.state.start('Game');
+    },
+
+    ViewControls : function() {
+       game.add.button(250,330,'controlkey', this.ViewControls, this);
     },
 
 }
