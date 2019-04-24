56e2e00821a387004d400edc0e067446906aca9e
Jongmin Lee <darkcruel12@naver.com>
2019-04-24 19:51:30 +0900

    refs #273 : add ending Wall_paper

--- public/js/ending.js
+++ public/js/ending.js
@@ -3,15 +3,17 @@ var easyRestart;
 var Ending = {
 
     preload : function() {
+    	game.load.image('Wall_paper' , 'img/space.jpg');
         game.load.image('restartButton', 'img/restartbutton.png');
         game.load.image('menuButton', 'img/menubutton.png');
     },
 
     create : function() {
-        game.stage.backgroundColor = '#FFF';
+        var image = game.add.image(0,0,'Wall_paper');
+        game.stage.background = image;
         game.add.button(350,300,'restartButton', this.startGame, this);
         game.add.button(350,400,'menuButton', this.goMenu, this);
-        totalScore = game.add.text(game.world.centerX, 200, score, { font: '124px Arial', fill: '#000' });
+        totalScore = game.add.text(game.world.centerX, 200, score, { font: '124px Arial', fill: '#00fs' });
         totalScore.anchor.setTo(0.5)
         easyRestart = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     },
