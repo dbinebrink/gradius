0b27287ca69a35f79aea3afd5eb0bae031f9f0a8
Jongmin Lee <darkcruel12@naver.com>
2019-04-24 11:21:42 +0900

    refs #411 : add wall_paper

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,13 +3,16 @@ var easyStart;
 var mainMenu = {
 
     preload : function() {
+
+        game.load.image('Wall_paper', 'img/space.jpg');
         game.load.image('startButton', 'img/startbutton.png');
         game.load.image('controls', 'img/controls.png');       
         game.load.image('controlkey', 'img/controlkey.png');
     },
 
     create : function() {
-        game.stage.backgroundColor = '#fff';
+        var image = game.add.image(0,0,'Wall_paper');
+        game.stage.background = image;
         game.add.button(350,270,'startButton', this.startGame, this);
         game.add.button(390,330,'controls', this.ViewControls, this);
         easyStart = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
