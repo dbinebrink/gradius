0762852fcbfb3e4983662dd6d3b1e5d77eb0ea29
박준영 <ib0712@naver.com>
2019-04-29 05:15:52 +0900

    controlkey-1.png

--- public/js/menu.js
+++ public/js/menu.js
@@ -7,7 +7,7 @@ var mainMenu = {
     	game.load.image('Wall_paper' , 'img/space.jpg');
         game.load.image('startButton', 'img/startbutton.png');
         game.load.image('controls', 'img/controls.png');       
-        game.load.image('controlkey', 'img/controlkey.png');
+        game.load.image('controlkey', 'img/controlkey-1.png');
         game.load.image('backButton', 'img/backButton.png');
         mainMenu.load.audio('start_sound', 'audio/start_sound.mp3')
     },
