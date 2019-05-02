f97ecc8bcc7a64b4cb08e3826ea4072743628494
dury008 <dury008@naver.com>
2019-05-03 01:55:41 +0900

    refs #556 : add exit button

--- public/js/menu.js
+++ public/js/menu.js
@@ -10,6 +10,7 @@ var mainMenu = {
         game.load.image('controls', 'img/controls.png');
         game.load.image('controlkey', 'img/controlkey-1.png');
         game.load.image('backButton', 'img/backButton.png');
+        game.load.image('exit' , 'img/exit.png');
         mainMenu.load.audio('start_sound', 'audio/start_sound.mp3')
     },
 
@@ -19,6 +20,7 @@ var mainMenu = {
         game.stage.background = image;
         game.add.button(game.world.centerX-220,280,'startButton', this.startGame, this);
         game.add.button(game.world.centerX+20,280,'controls', this.ViewControls, this);
+        game.add.button(game.world.centerX-110,400,'exit', this.exit, this);
         easyStart = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
         music = game.add.audio('start_sound');
         music.play();
@@ -37,8 +39,13 @@ var mainMenu = {
         this.state.start('Game');
     },
 
+
     ViewControls : function() {
        game.add.button(220,360,'controlkey', this.ViewControls, this);
        game.add.button(600,360,'backButton', this.create, this);
     },
+
+    exit : function(){
+      window.open('about:blank', '_self').close();
+    }
 }
