7d151fc05d47da074cd7284b4e21611174fd7968
Unknown <wogns3623@gmail.com>
2019-05-04 03:34:32 +0900

    modify item saving & loading
    함수를 문자열 형태로 저장하고 다시 불러올 때 eval을 사용해 다시 함수형으로 만들어주는 방식으로 item의 abilities를 저장하고 불러오는 방식을 변경하였습니다.

--- webserver/item/itemFormat.js
+++ webserver/item/itemFormat.js
@@ -36,7 +36,7 @@ class item {
     addAbility(type, appliedObj, value) {
         if(!item.getAbilityList(appliedObj).includes(type)) throw new UndefinedTypeError("undefined ability type!");
         if(!this.abilities[appliedObj][type]) this.abilities[appliedObj][type] = [];
-        this.abilities[appliedObj][type].push(value);
+        this.abilities[appliedObj][type].push(eval(value));
     }
 }
 
@@ -45,71 +45,67 @@ class item {
 // make laser item
 var laser_item = new item("laser", "uncommon");
 
-laser_item.addAbility("damage", "Bullets", x => x/3);
-laser_item.addAbility("fireRate", "Bullets", x => x*3);
-laser_item.addAbility("type", "Bullets", x => "laser");
-laser_item.addAbility("piercing", "Bullets", x => 0);
-laser_item.addAbility("firing", "Bullets", (bulletObj, currentBullet) => {
-    currentBullet.body.x = Player.sprite.body.x + bulletObj.info.initValue.position.x;
-    currentBullet.body.y = Player.sprite.body.y;
-});
-laser_item.addAbility("animation", "Bullets", obj => {
-    obj.animations.add('shootBeam', [0,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,4]);
-    return 'shootBeam';
-});
+laser_item.addAbility("damage", "Bullets", "x => x/3");
+laser_item.addAbility("fireRate", "Bullets", "x => x*3");
+laser_item.addAbility("type", "Bullets", "x => 'laser'");
+laser_item.addAbility("piercing", "Bullets", "x => 0");
+laser_item.addAbility("firing", "Bullets", "(bulletObj, currentBullet) => {\
+    currentBullet.body.x = Player.sprite.body.x + bulletObj.info.initValue.position.x;\
+    currentBullet.body.y = Player.sprite.body.y;\
+}");
+
+laser_item.addAbility("animation", "Bullets", "obj => {\
+    obj.animations.add('shootBeam', [0,0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,4]);\
+    return 'shootBeam';\
+}");
 
 // make pierce item
 var pierce_item = new item("pierce", "common");
 
-pierce_item.addAbility("piercing", "Bullets", x => {
-    if(x > 0) return x+1;
-    else return x;
-});
-
-// make bullet tracking item
-var tracking_item = new item("tracking", "uncommon");
-
-tracking_item.addAbility("firing", "Bullets", (bulletObj, currentBullet) => {
-    let enemy = aliens.getClosestTo({x:currentBullet.x - aliens.x,y:currentBullet.y-30});
-    // console.log(this.Bullets.info.bulletSpeed);
-    let trackingPerformance = 0.03;
-    let x = currentBullet.body.velocity.x+(enemy.body.x-currentBullet.x)*trackingPerformance;
-    let y = currentBullet.body.velocity.y+(enemy.body.y-currentBullet.y)*trackingPerformance;
-
-    let currentBulletSpeed = Math.sqrt( Math.pow(x,2)+Math.pow(y,2) );
-    let expectedBulletSpeed = bulletObj.info.bulletSpeed*100;
-    if ( currentBulletSpeed > expectedBulletSpeed ){
-        let reduceAmount = expectedBulletSpeed/currentBulletSpeed;
-        x = x*reduceAmount;
-        y = y*reduceAmount;
-    }
-
-    if(x == 0){
-        if(y > 0) currentBullet.rotation = Math.PI/2;
-        else currentBullet.rotation = -Math.PI/2;
-    }
-    else currentBullet.rotation = Math.atan(y/x);
-
-    currentBullet.body.velocity.x = x;
-    currentBullet.body.velocity.y = y;
-});
+pierce_item.addAbility("piercing", "Bullets", "x => {\
+    if(x > 0) return x+1;\
+    else return x;\
+}");
 
 // make double bullet fire item
 var addBullet_item = new item("addBullet", "common");
 
-addBullet_item.addAbility("fireAtOnce", "Bullets", x => x+1);
-addBullet_item.addAbility("beforeFire", "Bullets", (bulletObj, currentBullet) => {
-    // console.log(this);
-    bulletObj.info.initValue.position.x = 20;
-    bulletObj.info.initValue.position.y = (bulletObj.currentShotCount - (bulletObj.info.fireAtOnce-1)/2)*20;
-});
+addBullet_item.addAbility("fireAtOnce", "Bullets", "x => x+1");
+addBullet_item.addAbility("beforeFire", "Bullets", "(bulletObj, currentBullet) => {\
+    bulletObj.info.initValue.position.x = 20;\
+    bulletObj.info.initValue.position.y = (bulletObj.currentShotCount - (bulletObj.info.fireAtOnce-1)/2)*20;\
+}");
+
+// make bullet tracking item
+var tracking_item = new item("tracking", "uncommon");
+
+tracking_item.addAbility("firing", "Bullets", "(bulletObj, currentBullet) => {\
+    let enemy = aliens.getClosestTo({x:currentBullet.x - aliens.x,y:currentBullet.y-30});\
+    let trackingPerformance = 0.03;\
+    let x = currentBullet.body.velocity.x+(enemy.body.x-currentBullet.x)*trackingPerformance;\
+    let y = currentBullet.body.velocity.y+(enemy.body.y-currentBullet.y)*trackingPerformance;\
+    let currentBulletSpeed = Math.sqrt( Math.pow(x,2)+Math.pow(y,2) );\
+    let expectedBulletSpeed = bulletObj.info.bulletSpeed*100;\
+    if( currentBulletSpeed > expectedBulletSpeed ){\
+        let reduceAmount = expectedBulletSpeed/currentBulletSpeed;\
+        x = x*reduceAmount;\
+        y = y*reduceAmount;\
+    }\
+    if(x == 0){\
+        if(y > 0) currentBullet.rotation = Math.PI/2;\
+        else currentBullet.rotation = -Math.PI/2;\
+    }\
+    else currentBullet.rotation = Math.atan(y/x);\
+    currentBullet.body.velocity.x = x;\
+    currentBullet.body.velocity.y = y;\
+}");
 
 
 var fireRateUp_item = new item("fireRateUp", "common");
+fireRateUp_item.addAbility("fireRate", "Bullets", "x => x-0.1");
 
-fireRateUp_item.addAbility("fireRate", "Bullets", x => x-0.1);
 var playerSpeedUp_item = new item("speedUp", "common");
-playerSpeedUp_item.addAbility("speed", "Player", x => x+10);
+playerSpeedUp_item.addAbility("speed", "Player", "x => x+10");
 
 
 // var Shotgun_item = new item("Shotgun", "uncommon");
