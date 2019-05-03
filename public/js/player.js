da0f4ad73b17a6c569b1b9e30c8b9050ee26d704
Unknown <wogns3623@gmail.com>
2019-05-03 14:17:59 +0900

    Add liveCount into player.js

--- public/js/player.js
+++ public/js/player.js
@@ -17,12 +17,25 @@ var Player = {
             obj.animations.add('up', [3, 4], 2, false);
             obj.animations.add('down', [0, 1], 2, false);
         }
+
+        this.healthGroup = game.add.group();
+        for (var i = this.info.playerMaxHealth-1; i >= 0; i--) {
+            var ship = this.healthGroup.create(game.world.width - 150 + (60 * i), 60, this.info.playerImage);
+            ship.anchor.setTo(0.5, 0.5);
+            ship.angle = 0;
+            ship.alpha = 0.4;
+        }
+        game.add.text(game.world.width - 100, 10, 'Health: ', { font: '24px Arial', fill: '#fff' });
+
         this.sprite = game.add.sprite(150, 300, this.info.playerImage);
         game.physics.enable(sprite, Phaser.Physics.ARCADE);
+        this.sprite.body.collideWorldBounds = true;
         this.setPlayerSprite();
+        this.sprite.setHealth(this.info.playerMaxHealth);
     },
     
     setPlayerSprite : function(){
+        this.sprite.maxHealth = this.info.playerMaxHealth;
         this.sprite.anchor.setTo(0.5, 0.5);
         this.sprite.body.setSize(64,32,0,16);
         this.info.playerAnimation(this.sprite);
@@ -30,50 +43,48 @@ var Player = {
 
     damage : function(getDamage) {
         this.sprite.damage(getDamage);
-        live = lives.getChildAt(this.info.playerMaxHealth-live_count);
+        live = this.healthGroup.getChildAt(this.info.playerMaxHealth-this.sprite.health);
         live.alpha = 0;
-        live_count--;
     },
 
     move : function(cursors) {
-        if(cursors.left.isDown && (40 < this.sprite.x) && cursors.up.isDown && (40 < this.sprite.y)){
-            this.sprite.body.velocity.x = -this.info.playerSpeed * Math.sqrt(2) / 2;
-            this.sprite.body.velocity.y = -this.info.playerSpeed * Math.sqrt(2) / 2;
+        if(cursors.left.isDown && cursors.up.isDown) {
+            this.sprite.body.velocity.x = -this.info.playerSpeed * Math.SQRT2 / 2;
+            this.sprite.body.velocity.y = -this.info.playerSpeed * Math.SQRT2 / 2;
             this.sprite.animations.play('up');
         }
 
-        else if(cursors.left.isDown && (40 < this.sprite.x) && cursors.down.isDown && (this.sprite.y < 560)){
-            this.sprite.body.velocity.x = -this.info.playerSpeed * Math.sqrt(2) / 2;
-            this.sprite.body.velocity.y = this.info.playerSpeed * Math.sqrt(2) / 2;
+        else if(cursors.left.isDown && cursors.down.isDown) {
+            this.sprite.body.velocity.x = -this.info.playerSpeed * Math.SQRT2 / 2;
+            this.sprite.body.velocity.y = this.info.playerSpeed * Math.SQRT2 / 2;
             this.sprite.animations.play('down');
         }
-        else if(cursors.right.isDown && (this.sprite.x < 860) && cursors.up.isDown && (40 < this.sprite.y)){
-            this.sprite.body.velocity.x = this.info.playerSpeed * Math.sqrt(2) / 2;
-            this.sprite.body.velocity.y = -this.info.playerSpeed * Math.sqrt(2) / 2;
+        else if(cursors.right.isDown && cursors.up.isDown) {
+            this.sprite.body.velocity.x = this.info.playerSpeed * Math.SQRT2 / 2;
+            this.sprite.body.velocity.y = -this.info.playerSpeed * Math.SQRT2 / 2;
             this.sprite.animations.play('up');
         }
-        else if(cursors.right.isDown && (this.sprite.x < 860) && cursors.down.isDown && (this.sprite.y) < 560){
-            this.sprite.body.velocity.x = this.info.playerSpeed * Math.sqrt(2) / 2;
-            this.sprite.body.velocity.y = this.info.playerSpeed * Math.sqrt(2) / 2;
+        else if(cursors.right.isDown && cursors.down.isDown) {
+            this.sprite.body.velocity.x = this.info.playerSpeed * Math.SQRT2 / 2;
+            this.sprite.body.velocity.y = this.info.playerSpeed * Math.SQRT2 / 2;
             this.sprite.animations.play('down');
         }
 
-        else if (cursors.left.isDown && (40 < this.sprite.x)) {
+        else if (cursors.left.isDown) {
             this.sprite.body.velocity.x = -this.info.playerSpeed
         }
-        else if (cursors.right.isDown && (this.sprite.x < 860)) {
+        else if (cursors.right.isDown) {
             this.sprite.body.velocity.x = this.info.playerSpeed
         }
-
-        // keyboard up/down
-        else if (cursors.up.isDown && (40 < this.sprite.y)) {
+        else if (cursors.up.isDown) {
             this.sprite.body.velocity.y = -this.info.playerSpeed
             this.sprite.animations.play('up');
         }
-        else if (cursors.down.isDown && (this.sprite.y < 560)) {
+        else if (cursors.down.isDown) {
             this.sprite.body.velocity.y = this.info.playerSpeed
             this.sprite.animations.play('down');
         }
+
         else {  // stand still
             this.sprite.animations.stop();
             this.sprite.frame = 2;
