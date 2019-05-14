7faa0e48cf6f28d8fbd4650b95b07aab3eef821a
HyeongChang Lee <hc9904@hanyang.ac.kr>
2019-05-15 02:56:06 +0900

    refs #799 : Update shortcut (#800)

---
+++ runlocal.js
@@ -0,0 +1,21 @@
+let exec = require('child_process').exec;
+
+console.log("your platform is "+ process.platform);
+console.log("electron-packger will be run with [LOCAL] package");
+
+let command = "node ./node_modules/electron-packager/cli.js . --ignore=node_modules/electron-packager --ignore=.git --overwrite --ignore=\"\\.git(ignore|modules)\" --out=app --platform="
+let option = process.platform;
+
+console.log("Running electron-packager! Please wait!");
+
+exec(command + option,function (err, stdout, stderr) {
+  console.log("");
+  console.log("Finished!");
+  console.log("");
+  console.log("result --------------");
+  console.log(stdout);
+  console.log(stderr);
+  if (err !== null) {
+    console.log('error: ' + err);
+  }
+});
